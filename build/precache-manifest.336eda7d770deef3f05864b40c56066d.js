6f3accf70bc862ad54dcd6c662fc6f7816bdafaf
timbersaw4422 <timbersaw442211@gmail.com>
2020-07-09 13:58:00 +0300

    add build folder

---
+++ build/precache-manifest.336eda7d770deef3f05864b40c56066d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73bf670b07cb19c9e62fb1f5136dab5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eee3aec1f40db118049",
+    "url": "/static/css/main.409b5385.chunk.css"
+  },
+  {
+    "revision": "dcf41016e5903c36bef2",
+    "url": "/static/js/2.1434be1b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1434be1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eee3aec1f40db118049",
+    "url": "/static/js/main.62f4592c.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
